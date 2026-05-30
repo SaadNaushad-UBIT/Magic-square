315059915c61889bb19c4986d25c622faee854ee
SaadNaushad-UBIT <saadnaushad666@gmail.com>
2026-05-29 17:58:12 -0700

    test 2

--- Magic Square/magicsq.java
+++ Magic Square/magicsq.java
@@ -3,5 +3,6 @@ public class magicsq {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("why");
+        System.out.println("trying to learn git");
     }
 }
